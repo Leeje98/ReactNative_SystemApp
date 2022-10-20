7b360236b314c9aa4f1f837d406dc4d3f08665f5
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-20 13:16:31 +0900

    01. 화면과 네이게이션 자동 반영

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import {theme} from './theme'
+import Navigation from './navigations';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor={theme.background} barStyle='dark-content' />
+      <Navigation />
+    </ThemeProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+const Colors = {
+  white: '#ffffff',
+  black: '#111111',
+  main: '#3f8aec',
+};
+
+export const theme = {
+  background: Colors.white,
+  text: Colors.text,
+}
